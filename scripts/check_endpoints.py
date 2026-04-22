350204756e713a75967edb8f12eff64371b85859
Ayan-blue-team <salmananyas@gmail.com>
2026-04-22 02:15:06 -0700

    Update check_endpoints.py

--- scripts/check_endpoints.py
+++ scripts/check_endpoints.py
@@ -1,19 +1,1 @@
-import requests
-import urllib3
-urllib3.disable_warnings()
 
-QRADAR_IP = "18.212.3.25"
-TOKEN = "57e4bbc2-e588-4577-9904-0bc16beb7793"
-
-HEADERS = {
-    "SEC": TOKEN,
-    "Content-Type": "application/json",
-    "Version": "12.0",
-    "Accept": "application/json"
-}
-
-url = f"https://{QRADAR_IP}/api/analytics/rules"
-
-response = requests.get(url, headers=HEADERS, verify=False)
-print("Status:", response.status_code)
-print("Response:", response.text[:1000])
